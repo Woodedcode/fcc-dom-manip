05ee7dc73493b526e3c1e12ca2cf8acc1406ffba
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-12-04 15:21:52 -0600

    Update main.js

--- main.js
+++ main.js
@@ -23,3 +23,48 @@
 // console.log(item2)
 // const item2Img= item2.querySelector('img')
 // console.log(item2Img)
+
+
+// console.log('works')
+
+// console.log(window.location)
+// console.log(location)
+// console.log(innerHeight)
+// console.log(innerWidth)
+///////////////////////////////                DOM
+// console.log(document)
+// console.log(navigator)
+
+///////////////////////////////                SELECTING ELEMENTS
+
+// GETTERS
+// const p = document.querySelector('p')
+// const divs = document.querySelectorAll('div.item')
+// console.log(p)
+// console.log(divs)
+
+
+// ////////////// PARENT CHILD SELECTORS
+// const imgs = document.querySelector('.item img')
+// console.log(imgs)
+// const item2 = document.querySelector('.item2')
+// console.log(item2)
+// const item2Img= item2.querySelector('img')
+// console.log(item2Img)
+
+///////////////////////////////                ELEMENT PROPERTIES AND METHODS
+// const heading = document.querySelector('h2')
+// console.log(heading.textContent)
+// // heading.textContent = 'Ryan is cool'
+// // console.dir(heading.textContent)
+// // console.dir(heading.innerText)
+// console.log(heading.innerHTML)
+// console.log(heading.outerHTML)
+
+// ////////////// ADDING TO HTML WITHOUT HTML
+// const pizzaList = document.querySelector('.pizza')
+// console.log(pizzaList.textContent)
+// // pizzaList.textContent = `${pizzaList.textContent} 🍕`
+// ////////////// WITH INSERTADJACENTTEXT
+// pizzaList.insertAdjacentText('afterbegin', '🍕')
+// pizzaList.insertAdjacentText('beforeend', '🍕')
