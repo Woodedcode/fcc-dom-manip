e7a14af624e75ec01310ba3cb4913337d7d32bb0
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-12-04 10:55:15 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log('works')
+
+console.log(window.location)
+console.log(location)
+console.log(innerHeight)
+console.log(innerWidth)
+////////////////////////////DOM
+console.log(document)
+console.log(navigator)console.log('works')
