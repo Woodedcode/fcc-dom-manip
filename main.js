92ad36ab8f31aa5be515903c33a5a92f35a13086
Ryan <98045919+Woodedcode@users.noreply.github.com>
2024-12-04 14:13:46 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-console.log('works')
+// console.log('works')
 
-console.log(window.location)
-console.log(location)
-console.log(innerHeight)
-console.log(innerWidth)
-////////////////////////////DOM
-console.log(document)
-console.log(navigator)console.log('works')
+// console.log(window.location)
+// console.log(location)
+// console.log(innerHeight)
+// console.log(innerWidth)
+///////////////////////////////                DOM
+// console.log(document)
+// console.log(navigator)
+///////////////////////////////                SELECTING ELEMENTS
+
+// GETTERS
+// const p = document.querySelector('p')
+// const divs = document.querySelectorAll('div.item')
+// console.log(p)
+// console.log(divs)
+
+
+// ////////////// PARENT CHILD SELECTORS
+// const imgs = document.querySelector('.item img')
+// console.log(imgs)
+// const item2 = document.querySelector('.item2')
+// console.log(item2)
+// const item2Img= item2.querySelector('img')
+// console.log(item2Img)
